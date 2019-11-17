f8b12e1d4751f0e4be4d41fb1f5ab209750355c9
William Buck <wsbuckiii@gmail.com>
2019-11-17 04:36:31 -0800

    small changes

--- radio_days_client/src/screens/SongListScreen.js
+++ radio_days_client/src/screens/SongListScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import SongDetails from "../components/SongDetails";
 
+import guns_n_roses_image from "../assets/images/guns_n_roses.png";
+
 import {
   StyleSheet,
   FlatList,
@@ -17,30 +19,35 @@ const songs = [
     artist: "Rolling Stones",
     id: "1",
     file: require("../assets/music/gimme_shelter.mp3"),
+    albumArt: guns_n_roses_image,
   },
   {
     title: "Bohemiab Rhapsody",
     artist: "Queen",
     id: "2",
     url: music,
+    albumArt: guns_n_roses_image,
   },
   {
     title: "Sweet Child O' Mine",
     artist: "Guns N' Roses",
     id: "3",
     url: music,
+    albumArt: guns_n_roses_image,
   },
   {
     title: "Livin' On A Prayer",
     artist: "Bon Jovi",
     id: "4",
     url: music,
+    albumArt: guns_n_roses_image,
   },
   {
     title: "Kickstart My Heart",
     artist: "Motley Crue",
     id: "5",
     url: music,
+    albumArt: guns_n_roses_image,
   }
 ];
 
@@ -60,6 +67,7 @@ const SongListScreen = props => {
                   artist: item.artist,
                   songUrl: item.url,
                   file: item.file,
+                  albumArt: item.albumArt,
                 });
               }}
             >
